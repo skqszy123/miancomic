67feca3c3a4f9a082d3437c31de59e920f393519
Alix <1060211758@qq.com>
2016-10-17 10:37:53 +0800

    2333

--- app/src/main/java/com/hiroshi/cimoc/ui/activity/DetailActivity.java
+++ app/src/main/java/com/hiroshi/cimoc/ui/activity/DetailActivity.java
@@ -164,7 +164,6 @@ public class DetailActivity extends BaseActivity {
         mStarButton.setVisibility(View.VISIBLE);
         if (list.isEmpty()) {
             showSnackbar(R.string.detail_error);
-
         }
     }
     private String loadData(Context context) {
